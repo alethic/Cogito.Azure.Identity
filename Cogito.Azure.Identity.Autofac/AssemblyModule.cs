b6ac695001d93929a5973c12db183111a0c4b2b7
Jerome Haltom <jhaltom@alethic.solutions>
2022-02-19 18:27:54 -0600

    Update AssemblyModule.cs
    Allow configuration in Azure:Identity as well.

--- Cogito.Azure.Identity.Autofac/AssemblyModule.cs
+++ Cogito.Azure.Identity.Autofac/AssemblyModule.cs
@@ -14,6 +14,7 @@ namespace Cogito.Azure.Identity.Autofac
             builder.RegisterModule<Cogito.Extensions.Options.Configuration.Autofac.AssemblyModule>();
             builder.RegisterFromAttributes(typeof(AssemblyModule).Assembly);
             builder.Configure<AzureIdentityOptions>("AzureAd");
+            builder.Configure<AzureIdentityOptions>("Azure:Identity");
             builder.RegisterType<AzureIdentityOptionsCredential>().AsSelf();
             builder.RegisterType<AzureIdentityCredential>().AsSelf();
         }
