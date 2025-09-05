85121fd4ea5e0483f64debda600e0bd4069a6cba
Jerome Haltom <jhaltom@alethic.solutions>
2025-09-05 15:09:00 -0500

    Remove setup of credential using clone, since that ignores new options added.

--- src/Cogito.Azure.Identity/AzureIdentityCredential.cs
+++ src/Cogito.Azure.Identity/AzureIdentityCredential.cs
@@ -22,59 +22,7 @@ namespace Cogito.Azure.Identity
         /// <returns></returns>
         static DefaultAzureCredentialOptions CreateDefaultOptions(AzureIdentityOptions options, DefaultAzureCredentialOptions source)
         {
-            var o = new DefaultAzureCredentialOptions();
-
-            if (source != null)
-            {
-                o.ExcludeAzureCliCredential = source.ExcludeAzureCliCredential;
-                o.ExcludeAzurePowerShellCredential = source.ExcludeAzurePowerShellCredential;
-                o.ExcludeEnvironmentCredential = source.ExcludeEnvironmentCredential;
-                o.ExcludeInteractiveBrowserCredential = source.ExcludeInteractiveBrowserCredential;
-                o.ExcludeManagedIdentityCredential = source.ExcludeManagedIdentityCredential;
-                o.ExcludeSharedTokenCacheCredential = source.ExcludeSharedTokenCacheCredential;
-                o.ExcludeVisualStudioCodeCredential = source.ExcludeVisualStudioCodeCredential;
-                o.ExcludeVisualStudioCredential = source.ExcludeVisualStudioCredential;
-
-                if (source.ManagedIdentityClientId != null)
-                    o.ManagedIdentityClientId = source.ManagedIdentityClientId;
-
-                if (source.SharedTokenCacheTenantId != null)
-                    o.SharedTokenCacheTenantId = source.SharedTokenCacheTenantId;
-
-                if (source.SharedTokenCacheUsername != null)
-                    o.SharedTokenCacheUsername = source.SharedTokenCacheUsername;
-
-                if (source.InteractiveBrowserTenantId != null)
-                    o.InteractiveBrowserTenantId = source.InteractiveBrowserTenantId;
-
-                if (source.VisualStudioTenantId != null)
-                    o.VisualStudioTenantId = source.VisualStudioTenantId;
-
-                if (source.VisualStudioCodeTenantId != null)
-                    o.VisualStudioCodeTenantId = source.VisualStudioCodeTenantId;
-
-                if (source.Transport != null)
-                    o.Transport = source.Transport;
-
-                if (source.AuthorityHost != null)
-                    o.AuthorityHost = source.AuthorityHost;
-            }
-
-            if (options != null)
-            {
-                if (options.Instance != null)
-                    o.AuthorityHost = new Uri(options.Instance);
-
-                if (options.TenantId != null)
-                {
-                    o.SharedTokenCacheTenantId = options.TenantId;
-                    o.InteractiveBrowserTenantId = options.TenantId;
-                    o.VisualStudioTenantId = options.TenantId;
-                    o.VisualStudioCodeTenantId = options.TenantId;
-                }
-            }
-
-            return o;
+            return source ?? new DefaultAzureCredentialOptions();
         }
 
         /// <summary>
