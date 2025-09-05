f280419d12977cfdadc22ebfc61dc1722f75254f
Jerome Haltom <jhaltom@alethic.solutions>
2025-09-05 15:04:15 -0500

    Remove AzureAd configuration section.

--- src/Cogito.Azure.Identity.Autofac/AssemblyModule.cs
+++ src/Cogito.Azure.Identity.Autofac/AssemblyModule.cs
@@ -15,7 +15,6 @@ namespace Cogito.Azure.Identity.Autofac
         {
             builder.RegisterModule<Cogito.Extensions.Options.Configuration.Autofac.AssemblyModule>();
             builder.RegisterFromAttributes(typeof(AssemblyModule).Assembly);
-            builder.Configure<AzureIdentityOptions>("AzureAd");
             builder.Configure<AzureIdentityOptions>("Azure:Identity");
             builder.RegisterType<AzureIdentityOptionsCredential>().AsSelf();
             builder.RegisterType<AzureIdentityCredential>().AsSelf().As<TokenCredential>();
